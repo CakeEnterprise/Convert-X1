5a683f508ca24479a3dbd7e4214595a95f3bc848
maomao <tangpansak@gmail.com>
2021-03-31 20:57:10 +0700

    change self.data to dunder variable

--- src/func.py
+++ src/func.py
@@ -1,21 +1,22 @@
 class PNG:
     def __init__(self,filename):
         self.filename = filename
-        self.data = self.__read_file__(filename)
+        self.__data = self.__read_file__(filename)
 
     def __read_file__(self,filename):
         from PIL import Image
         return Image.open(filename)
 
     def to_jpg(self,filename=None):
-        self.data = self.data.convert("RGB")
+        self.__data = self.__data.convert("RGB")
         if filename is not None:
-            self.data.save(filename,"jpeg")
+            self.__data.save(filename,"jpeg")
         else:
-            self.data.save(self.filename.replace(".png",".jpg"),"jpeg")
-
+            self.__data.save(self.filename.replace(".png",".jpg"),"jpeg")
+    
+    # utility function to change image data
     def grayscale(self):
-        self.data = self.data.convert("L")
+        self.__data = self.__data.convert("L")
 
     def black_and_white(self):
         # lambda function to convert each point to black or white
@@ -35,3 +36,10 @@ class JPEG:
         else:
             self.__data.save(self.filename.replace(".jpg", ".png"), "png")
 
+
+if __name__ == "__main__":
+    png = PNG("eliteweb.png")
+    png.grayscale()
+    png.to_jpg("elitewebg.jpg")
+    # jpg = JPEG("eliteweb.jpg")
+    # jpg.to_png("hello.png")
