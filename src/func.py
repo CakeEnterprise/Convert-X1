1225727adeb1058baf788173d8bcab4dd7d0d27f
maomao <tangpansak@gmail.com>
2021-03-31 20:13:38 +0700

    initial class of PNG image

---
+++ src/func.py
@@ -0,0 +1,18 @@
+class PNG:
+    def __init__(self,filename):
+        self.filename = filename
+        self.data = self.__read_file__(filename)
+
+    def __read_file__(self,filename):
+        from PIL import Image
+        return Image.open(filename)
+
+    def to_jpg(self,filename=None):
+        self.data = self.data.convert("RGB")
+        if filename is not None:
+            self.data.save(filename,"jpeg")
+        else:
+            self.data.save(self.filename.replace(".png",".jpg"),"jpeg")
+
+    def grayscale(self):
+        self.data = self.data.convert("greyscale")
