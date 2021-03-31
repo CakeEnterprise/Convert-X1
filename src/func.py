47e38c3f1dac4aa32e1e943519b46ac7b730edb0
maomao <tangpansak@gmail.com>
2021-03-31 20:53:50 +0700

    initial comit for jpeg class

--- src/func.py
+++ src/func.py
@@ -20,3 +20,18 @@ class PNG:
     def black_and_white(self):
         # lambda function to convert each point to black or white
         self.__data = self.__data.point(lambda x: 0 if x < 128 else 255, '1')
+
+class JPEG:
+    def __init__(self, filename):
+        self.filename = filename
+        self.__data = self.__read_file__(filename)
+
+    def __read_file__(self, filename):
+        from PIL import Image
+        return Image.open(filename)
+    def to_png(self, filename=None):
+        if filename is not None:
+            self.__data.save(filename, "png")
+        else:
+            self.__data.save(self.filename.replace(".jpg", ".png"), "png")
+
